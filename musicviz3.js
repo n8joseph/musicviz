c60567f47a8093b30d51ea79a6e3262f9b4f4aac
n8joseph <n8joseph@users.noreply.github.com>
2015-05-16 14:35:53 -0400

    Update musicviz3.js

--- musicviz3.js
+++ musicviz3.js
@@ -4,31 +4,37 @@ function loadSong(url) {
 
   /* --- set up web audio --- */
   //create the context
-  var context = new AudioContext();
+  this.context = new AudioContext();
   //...and the source
-  var source = context.createBufferSource();
+  this.source = this.context.createBufferSource();
   //connect it to the destination so you can hear it.
-  source.connect(context.destination);
+  this.source.connect(this.context.destination);
 
   /* --- load up that buffer ---  */
   //Basic start to ajax! (I say basic, yet i don't know it well.)
-  var request = new XMLHttpRequest();
+  this.request = new XMLHttpRequest();
   //open the request...?
-  request.open('GET', url, true); 
+  this.request.open('GET', url, true); 
   //I don't even know.
-  request.responseType = 'arraybuffer';
+  this.request.responseType = 'arraybuffer';
   //Once the request has completed... do this
-  request.onload = function() {
-    context.decodeAudioData(request.response, function(response) {
+  
+}
+
+function playSong(){
+
+request.onload = function() {
+    loadSong.context.decodeAudioData(loadSong.request.response, function(response) {
   
       /* --- play the sound AFTER we've gotten the buffer loaded --- */
       //set the buffer to the response we just received.
-      source.buffer = response;
+      loadSong.source.buffer = response;
       //And off we go! .start(0) should play asap.
-      source.start(0);
+      loadSong.source.start(0);
     }, function () { console.error('The request failed.'); } );
   }
-  request.send();
+  loadSong.request.send();
 }
+
 window.addEventListener("load", loadSong('citt.mp3'));
 
