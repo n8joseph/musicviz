97d5491040b11d4ef4c71dc318acc17601be9409
n8joseph <n8joseph@users.noreply.github.com>
2015-05-17 20:18:02 -0400

    Update musicviz3.js

--- musicviz3.js
+++ musicviz3.js
@@ -16,8 +16,6 @@
   request.responseType = 'arraybuffer';
   //Once the request has completed... do this
 
-var startOffset = 0;
-
 function playSong(){
 
 request.onload = function() {
@@ -27,14 +25,14 @@ request.onload = function() {
       //set the buffer to the response we just received.
       source.buffer = response;
       //And off we go! .start(0) should play asap.
-      source.start(0, startOffset % buffer.duration);
+      source.start(0);
     }, function () { console.error('The request failed.'); } );
   }
   request.send();
 };
 
 // Assume context is a web audio context, buffer is a pre-loaded audio buffer.
-
+var startOffset = 0;
 var startTime = 0;
 
 function pause() {
@@ -42,14 +40,3 @@ function pause() {
   // Measure how much time passed since the last pause.
   startOffset += context.currentTime - startTime;
 };
-
-function play() {
-  startTime = context.currentTime;
-  var source = context.createBufferSource();
-  // Connect graph
-  source.buffer = this.buffer;
-  source.loop = true;
-  source.connect(context.destination);
-  // Start playback, but make sure we stay in bound of the buffer.
-  source.start(0, startOffset % buffer.duration);
-}
