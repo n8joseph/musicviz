7f8a1e4236eaec2c623d891e5191fce5ecd51e63
n8joseph <n8joseph@users.noreply.github.com>
2015-05-20 01:01:36 -0400

    Update jquer123.js

--- jquer123.js
+++ jquer123.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-       $('$slick-toggle').click(function() {
+       $('#slick-toggle').click(function() {
               $('img', this).attr('src', function(i, oldSrc) {
                             return oldSrc == 'http://cdn.flaticon.com/png/256/58315.png' ? 'http://cdn.flaticon.com/png/256/54505.png' : 'http://cdn.flaticon.com/png/256/58315.png';
               });
