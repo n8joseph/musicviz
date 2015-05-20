4a4ae683d8a4b55c31132143e878444eea09294d
n8joseph <n8joseph@users.noreply.github.com>
2015-05-20 01:14:53 -0400

    Update jquer123.js

--- jquer123.js
+++ jquer123.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
        $('#slick-toggle').click(function() {
               $('img', this).attr('src', function(i, oldSrc) {
-                            return oldSrc == 'http://cdn.flaticon.com/png/256/58315.png' ? 'http://cdn.flaticon.com/png/256/54505.png' : 'http://cdn.flaticon.com/png/256/58315.png';
+                            return oldSrc == 'http://oi57.tinypic.com/i4lfzc.jpg' ? 'http://oi59.tinypic.com/11qh1kg.jpg' : 'http://oi57.tinypic.com/i4lfzc.jpg';
               });
               $('#slickbox').toggle(400);
               return false;
