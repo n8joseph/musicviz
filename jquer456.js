1827b811c6a1344cf1ee8148e5e130a59cd76a13
n8joseph <n8joseph@users.noreply.github.com>
2015-05-20 23:12:59 -0400

    Update jquer456.js

--- jquer456.js
+++ jquer456.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
   $("#theplaybar").hover(function () {
-                    $(this).animate({'opacity':'0.7'}, 100);
+                    $(this).animate({'opacity':'0.9'}, 100);
               },
               function (){
-                    $(this).animate({'opacity':'0'}, 100);
+                    $(this).animate({'opacity':'0.05'}, 100);
              });
   
   });
