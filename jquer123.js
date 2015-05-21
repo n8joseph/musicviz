d8c8dcd8a86366c33de0cbe2a9dbb728a812fd24
n8joseph <n8joseph@users.noreply.github.com>
2015-05-20 22:37:56 -0400

    Update jquer123.js

--- jquer123.js
+++ jquer123.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
+      
        $('#slick-toggle').click(function() {
               $('img', this).attr('src', function(i, oldSrc) {
                             return oldSrc == 'http://oi57.tinypic.com/i4lfzc.jpg' ? 'http://oi59.tinypic.com/11qh1kg.jpg' : 'http://oi57.tinypic.com/i4lfzc.jpg';
               });
               $('#slickbox').toggle(400);
               return false;
+       });
+       
+      $('#playbar').mouseover(function() {
+             $('#playbar').hover(function() {
+                     $('#playbar').fadeTo(0, 0.2, function() {} )
+             },      
+             function() {
+                    $('#playbar').fadeTo(0, 0.02, function() {} )
+             }
+              });
        });
 });
